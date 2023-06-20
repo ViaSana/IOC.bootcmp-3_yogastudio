254c39c813bcac8d0a0f78dee787e052c9231a5b
playfluent <joe@playfluent.com>
2023-06-20 15:37:23 +0800

    new animations

--- main.js
+++ main.js
@@ -11,45 +11,36 @@ const burgerMenu = document.querySelector('.burger-menu')
 const burgerLines = document.querySelector('.burger-line')
 
 
+let tl = gsap.timeline({ duration:1, }
+    ).to(burgerMenu, {width: "23rem", opacity: 1, padding: '0 2rem',}
+    ).to('.burger-menu a', {opacity: 1,
+    //stagger:.35
+    })
+
+tl.pause();
+
+
 // then we start righting our new function
 function toggleBurgerMenu() {
-// log the click just to check its working. feel free to remove after you are done
-console.log('click')
+  
   // here we check to see if the burger button has the class of active
     if (burgerButton.classList.contains("active")){
         // more consle.log for testing
-        console.log('has active')
+        console.log('Burger Button is active, DEACTIVATE')
         
         // remove the class of active
         burgerButton.classList.remove("active")
-        
-        // gsap to close the email
-        gsap.to(burgerMenu, {
-        // width to shrink it
-            width: 0,
-        //padding to minimilistic so it wont overflow
-            padding: '0 0',
-        }),
-        gsap.to(burgerLines, {
-            opacity: 1,
-        })
+ 
+        tl.reverse()
 
     } else {
         // console for more testing
-        console.log('no has active')
+        console.log('Burger Button ACTIVATE')
         
         // re-adds the class for tracking
         burgerButton.classList.add("active")
         // opens up the burger menu and does the opsite of the close
-        gsap.to(burgerMenu, {
-            width: "26rem",
-            opacity: 1,
-            padding: '0 2.2rem'
-        }),
-        gsap.to(burgerLines, {
-            opacity: 0,
-            duration: 0.01,
-        })
+        tl.play()
     }
 }
 
@@ -69,18 +60,20 @@ headings.forEach(object => {
     // here's where we start attching the animation to the scroll
       scrollTrigger: {
       	// this is what is triggering the start of the animation
-          trigger: object,
-        	// this one takes in to bits of info. the first is the part of the trigger element thats causing the trigger to start and the second one is where on the screen. both of them can be either top, center, bottom or a percentage (% are from the top of the container( as in screen or trigger element) )   
-          start: "top center",
-			// same as start but the end point of the animation          
-          end: "bottom bottom",
-          // now this is the most important line of code its what turns the triggers from a start point in to locking it to the scroll
-          scrub: true,
-          // last but not least this helps us to visualize the script
-          markers: true,
+        trigger: object,
+        // this one takes in to bits of info. the first is the part of the trigger element thats causing the trigger to start and the second one is where on the screen. both of them can be either top, center, bottom or a percentage (% are from the top of the container( as in screen or trigger element) )   
+        start: "-90% 80%",
+		// same as start but the end point of the animation          
+        end: "center bottom",
+        // now this is the most important line of code its what turns the triggers from a start point in to locking it to the scroll
+        scrub: true,
+        // last but not least this helps us to visualize the script
+        markers: true,
       }
     }).from(object,{
         y:300,
         opacity:0
+    }).from(object,{
+        rotate:360,
     })
 })
